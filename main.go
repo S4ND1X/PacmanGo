4f182e3cbdc15d34b02d6b723fa06b30c0e7d5df
Jorge Sanchez <sandix390@gmail.com>
2021-11-30 10:28:14 -0600

    add player movement

--- main.go
+++ main.go
@@ -1,120 +1,189 @@
 package main
 
 import (
-    "bufio"
-    "fmt"
-    "log"
-    "os"
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"os/exec"
+
 	"github.com/danicat/simpleansi"
 )
 
+type sprite struct {
+	row int
+	col int
+}
 
-
-// Load maze and print
-
+var player sprite
 var maze []string
 
 func loadMaze(file string) error {
-    f, err := os.Open(file)
-    if err != nil {
-        return err
-    }
-    defer f.Close()
+	f, err := os.Open(file)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
 
-    scanner := bufio.NewScanner(f)
-    for scanner.Scan() {
-        line := scanner.Text()
-        maze = append(maze, line)
-    }
+	scanner := bufio.NewScanner(f)
+	for scanner.Scan() {
+		line := scanner.Text()
+		maze = append(maze, line)
+	}
 
-    return nil
+	for row, line := range maze {
+		for col, char := range line {
+			switch char {
+			case 'P':
+				player = sprite{row, col}
+			}
+		}
+	}
+
+	return nil
 }
-
 
 func printScreen() {
-    simpleansi.ClearScreen()
-    for _, line := range maze {
-        fmt.Println(line)
-    }
-}
+	simpleansi.ClearScreen()
+	for _, line := range maze {
+		for _, chr := range line {
+			switch chr {
+			case '#':
+				fmt.Printf("%c", chr)
+			default:
+				fmt.Print(" ")
+			}
+		}
+		fmt.Println()
+	}
 
+	simpleansi.MoveCursor(player.row, player.col)
+	fmt.Print("P")
 
-// Player Input
-
-func initialise() {
-    cbTerm := exec.Command("stty", "cbreak", "-echo")
-    cbTerm.Stdin = os.Stdin
-
-    err := cbTerm.Run()
-    if err != nil {
-        log.Fatalln("unable to activate cbreak mode:", err)
-    }
-}
-
-func cleanup() {
-    cookedTerm := exec.Command("stty", "-cbreak", "echo")
-    cookedTerm.Stdin = os.Stdin
-
-    err := cookedTerm.Run()
-    if err != nil {
-        log.Fatalln("unable to restore cooked mode:", err)
-    }
+	// Move cursor outside of maze drawing area
+	simpleansi.MoveCursor(len(maze)+1, 0)
 }
 
 func readInput() (string, error) {
-    buffer := make([]byte, 100)
+	buffer := make([]byte, 100)
 
-    cnt, err := os.Stdin.Read(buffer)
-    if err != nil {
-        return "", err
-    }
+	cnt, err := os.Stdin.Read(buffer)
+	if err != nil {
+		return "", err
+	}
 
-    if cnt == 1 && buffer[0] == 0x1b {
-        return "ESC", nil
-    }
+	if cnt == 1 && buffer[0] == 0x1b {
+		return "ESC", nil
+	} else if cnt >= 3 {
+		if buffer[0] == 0x1b && buffer[1] == '[' {
+			switch buffer[2] {
+			case 'A':
+				return "UP", nil
+			case 'B':
+				return "DOWN", nil
+			case 'C':
+				return "RIGHT", nil
+			case 'D':
+				return "LEFT", nil
+			}
+		}
+	}
 
-    return "", nil
+	return "", nil
 }
 
+func makeMove(oldRow, oldCol int, dir string) (newRow, newCol int) {
+	newRow, newCol = oldRow, oldCol
 
+	switch dir {
+	case "UP":
+		newRow = newRow - 1
+		if newRow < 0 {
+			newRow = len(maze) - 1
+		}
+	case "DOWN":
+		newRow = newRow + 1
+		if newRow == len(maze) {
+			newRow = 0
+		}
+	case "RIGHT":
+		newCol = newCol + 1
+		if newCol == len(maze[0]) {
+			newCol = 0
+		}
+	case "LEFT":
+		newCol = newCol - 1
+		if newCol < 0 {
+			newCol = len(maze[0]) - 1
+		}
+	}
 
+	if maze[newRow][newCol] == '#' {
+		newRow = oldRow
+		newCol = oldCol
+	}
+
+	return
+}
+
+func movePlayer(dir string) {
+	player.row, player.col = makeMove(player.row, player.col, dir)
+}
+
+func initialise() {
+	cbTerm := exec.Command("stty", "cbreak", "-echo")
+	cbTerm.Stdin = os.Stdin
+
+	err := cbTerm.Run()
+	if err != nil {
+		log.Fatalln("unable to activate cbreak mode:", err)
+	}
+}
+
+func cleanup() {
+	cookedTerm := exec.Command("stty", "-cbreak", "echo")
+	cookedTerm.Stdin = os.Stdin
+
+	err := cookedTerm.Run()
+	if err != nil {
+		log.Fatalln("unable to activate cooked mode:", err)
+	}
+}
 
 func main() {
-    // initialise game
-    initialise()
-    defer cleanup()
-    // load resources
-    err := loadMaze("maze01.txt")
-    if err != nil {
-        log.Println("failed to load maze:", err)
-        return
-    }
+	// initialise game
+	initialise()
+	defer cleanup()
 
-    // game loop
-    for {
-        // update screen
-        printScreen()
+	// load resources
+	err := loadMaze("maze01.txt")
+	if err != nil {
+		log.Println("failed to load maze:", err)
+		return
+	}
 
-        // process input
+	// game loop
+	for {
+		// update screen
+		printScreen()
+
+		// process input
 		input, err := readInput()
 		if err != nil {
-			log.Print("error reading input:", err)
+			log.Println("error reading input:", err)
 			break
 		}
 
-        // process movement
+		// process movement
+		movePlayer(input)
 
-        // process collisions
+		// process collisions
 
-        // check game over
+		// check game over
 		if input == "ESC" {
 			break
 		}
 
-        // Temp: break infinite loop
-        break
-
-        // repeat
-    }
+		// repeat
+	}
 }
-
